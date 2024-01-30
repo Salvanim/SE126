b360382dee2e61b1cf7dd3733c69c26a9ea4f57e
Fisher <djfisher@email.neit.edu>
2024-01-30 11:42:11 -0500

    lab2b

--- week3/demo/lab2b.py
+++ week3/demo/lab2b.py
@@ -1,4 +1,4 @@
-#Dylan Fisher: W3D1 Demo - text file handling & storying to 1D lists
+#Dylan Fisher: Lab#3 Homework : W3D1 Demo - text file handling & storying to 1D lists
 
 #Prompt:
     #Rewrite the Voter Registration Lab utilizing the data from the file voters.csv.  Store the field data into respective 1D lists, and then process the lists to determine the 4 final output values (make sure they are clearly labeled!). This final solution should have NO input() statements and when the console is ran it should print all 4 totals (you may reprint the data from the lists/fie if you would like)  Use your original Voter Registration Lab (or the solution file!) as starter code, but edit it to connect to a file and store data into lists, then use a for loop to process each voter and their data to find the 4 totals.
