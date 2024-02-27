fb244eeb9ef7634556c1aa1319cba545012ae95f
Salvanim <dylanfish05@icloud.com>
2024-02-27 14:31:20 -0500

    lab5

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -8,16 +8,16 @@ import csv
 
     #recordTotal: defines amount of records
 
-    
+
 #---MAIN PROGRAM--------------------------------------------------------------------
 
 #function for selecting option
 def optionSelector(options):
     print("\nThese are your searching options: ")
-        
+
     for i in range(0, len(options)):
         print(f"\n{i+1}. {options[i]}")
-        
+
     selectedOption = ""
     while not selectedOption.isdigit():
 
@@ -28,7 +28,7 @@ def optionSelector(options):
             if not (selectedOption in list(range(1, len(options)+1))):
                 print("\nNot a option, please try again: ")
                 selectedOption = ""
-                
+
         else:
             print("\nNot a number, please try again: ")
 
@@ -36,7 +36,7 @@ def optionSelector(options):
 
     selectedOption = int(selectedOption)
     return selectedOption
-    
+
 def menu():
     options = [
         "See All Student Report",
@@ -48,9 +48,9 @@ def menu():
     return optionSelector(options)
 
 
-    
+
 def seqSearch(searchTerm, searchLists):
-    
+
     found = [] #allows for multiples in search
     seq_count = 0
     #for loop allows review of each value in list (sequence)
@@ -66,16 +66,15 @@ def seqSearch(searchTerm, searchLists):
     return found
 
 def binarySearch(searchTerm, searchList):
-    searchTerm = input("\nWhat do you want to search for?: ")
-    min = 0 #first position of the list to be searched (ascending) 
+    min = 0 #first position of the list to be searched (ascending)
     max = len(searchList) - 1 #last position of the list to be searched (ascending)
     #mid = int((0 + (len(searchList) - 1)) / 2)
     mid = int((min + max) / 2)
     bin_count = 0
     #this is for INCREASING (ascending) order
-    while (min < max and searchTerm.lower() != searchList[mid].lower()):
+    while (min < max and str(searchTerm.lower()) != str(searchList[mid]).lower()):
         bin_count += 1
-        if searchTerm.lower() < searchList[mid].lower():
+        if str(searchTerm).lower() < str(searchList[mid]).lower():
             max = mid - 1
         else:
             min = mid + 1
@@ -83,13 +82,14 @@ def binarySearch(searchTerm, searchList):
         mid = int((min + max) / 2)
     return mid
 
+#function that asks for search term based on context and preforms binary search, then prints result
 def compleatBinarySearch(context, searchList, lists, spacing):
     searchQuestion = input(f"\nWhat {context} do you want to search for?: ")
     mid = binarySearch(searchQuestion, searchList)
-    printList = ""
-    for i in range(0, lists):
-        print(f"{lists[i][mid]:^{spacing[i]}} \t {lists[i][mid]:^{spacing[i]}} \t {lists[i][mid]:^{spacing[i]}} \t {lists[i][mid]:^{spacing[i]}} \t {lists[i][mid]:^{spacing[i]}} \t {lists[i][mid]:^{spacing[i]}}")
-
+    printListString = ""
+    for i in range(0, len(lists)):
+        printListString += f"{str(lists[i][mid]):8} "
+    print("\n"+printListString+"\n")
 
 #Varribles:
 recordTotal = 0
@@ -107,7 +107,7 @@ with open("lab5\lab5_students.txt") as csvfile:
     file = csv.reader(csvfile)
 
     for rec in file:
-    
+
         id_stud.append(int(rec[0]))
         lname.append(rec[1])
         fname.append(rec[2])
@@ -119,24 +119,34 @@ with open("lab5\lab5_students.txt") as csvfile:
 searchFile = 'y'
 while searchFile == 'y':
 
+    #Defines choice and runs diffrent programs based on choice
     choice = int(menu())
     if choice == 1:
-
+        #prints entire file
         print(f"\n {'ID':5} \t {'Last Name':8} \t {'First Name':10} \t {'Class 1':8} \t {'Class 2':8} \t {'Clsss 3':8}")
-    
+
         for i in range(0, recordTotal):
             print(f"{id_stud[i]:5} \t {lname[i]:8} \t {fname[i]:10} \t {class1[i]:8} \t {class2[i]:8} \t {class3[i]:8}")
-    
+
     elif choice == 2:
+        #binary search of ID
         compleatBinarySearch("ID", id_stud, [id_stud, lname, fname, class1, class2, class3], [5, 8, 10, 8, 8, 8])
-    
+
     elif choice == 3:
+        #binary search of last name
         compleatBinarySearch("last name", lname, [id_stud, lname, fname, class1, class2, class3], [5, 8, 10, 8, 8, 8])
-    
+
     elif choice == 4:
+        #prints list of people with class
         classToSearch = input("\nWhat class would you want to search for?: ")
         foundStudents = seqSearch(classToSearch, [class1, class2, class3])
 
+        print(f"\n {'ID':5} \t {'Last Name':8} \t {'First Name':10} \t {'Class 1':8} \t {'Class 2':8} \t {'Clsss 3':8}")
+
+        for val in foundStudents:
+            print(f"\n {id_stud[val]:5} \t {lname[val]:8} \t {fname[val]:10} \t {class1[val]:8} \t {class2[val]:8} \t {class3[val]:8}")
     else:
         searchFile = 'n'
         print("GoodBye!")
+
+    print("\n")
---
+++ week8/classLab.py
@@ -0,0 +1,76 @@
+#Dylan Fisher, Idrees Abdurrazzak, Latoya Hall
+#Generates seats 2d array
+seats = []
+for i in range(0, 7):
+    seats.append(["A", "B", "C", "D"])
+
+#function allows user to select row and seat for flight
+def selectSeat(seats):
+    #Prints array
+    printSeats(seats)
+
+    #loops and checks if user is getting seat placement
+    selectSeatPlacement = "y"
+    while selectSeatPlacement == "y":
+        #Asks user for seat row, and makes sure that row is not greater then largest amount fo rows
+        #Makes sure that value is number and within range
+        seatRow = -1
+        while seatRow not in range(0, len(seats)):
+            try:
+                seatRow = int(input("What row do you want to sit in?: "))-1
+            except:
+                print("ROW must be a Integer")
+
+            if seatRow not in range(0, len(seats)):
+                print(f"ROW must be greater than 0 and less than or equal to {len(seats)}")
+
+        isInproperLetter = "y"
+        while isInproperLetter == "y":
+            #asks user for seat letter
+            seatLetter = input("What seat do you want?: ")
+
+            #converts seat letter to index
+            letterDictionary = {"A": 0, "B":1, "C":2, "D":3}
+            if seatLetter in letterDictionary.keys:
+                isInproperLetter = 'n'
+                print("Not a proper Seat")
+
+        #gets seat user has slected
+        selectedSeat = seats[seatRow][letterDictionary[seatLetter]]
+
+        #checks if seat has been slected already
+        if selectedSeat != "X":
+            #sets seat at position to X
+            seats[seatRow][letterDictionary[seatLetter]] = "X"
+            printSeats(seats)
+            # asks user if the want to select another seat
+            another = input("Would you like to select another seat? (y/n): ")
+            if another != "y":
+                seatselectSeatPlacement = "n"
+        else:
+            print("Select another seat: ")
+    return seats
+
+#Itterates though seat array and prints 2d
+def printSeats(seats):
+    print("AirDrogon ~ Valyria Bount Flight #4815")
+    #prints top boarder and heading
+    print("-"*39)
+    print("|\tRow #\t-  -\t\t-  -  |")
+
+    #itterates though rows
+    for i in range(0, len(seats)):
+        #builds base starting string for row
+        rowString = "|\t " + str(i+1) + "\t"
+        #itterates though seat letters
+        for seat in range(0, len(seats[i])):
+            #changes spacing in rowString
+            if seat % 2 == 1 and seat != 3:
+                rowString += seats[i][seat] + "\t\t"
+            else:
+                rowString += seats[i][seat] + "  "
+        rowString += "|"
+        print(rowString)
+    print("-"*39)
+
+printSeats(selectSeat(seats))
